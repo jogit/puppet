7dbee67023557e29814659f22b7a6e85aa065393
jogit <jerome.rabouille@gmail.com>
2015-03-17 11:24:13 +0100

    suppression de node.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,27 +1,25 @@
 #Maintenu par JRA
 filebucket { 'main': server => 'n-puppet-01.stgibm.univ-fcomte.fr' }
 File { backup => 'main' }
-import "nodes"
+Exec { path => "/bin:/sbin:/usr/bin:/usr/sbin" }
+
+
 import "logiciels"
 import "fichiers"
 
-Exec { path => "/bin:/sbin:/usr/bin:/usr/sbin" }
 
-#class { 'ntp':
-#        server_list => [ 'chronos.iut-bm.univ-fcomte.fr' ]
-#    }
-
-class { 'nrpe':
+node basenode {
+  include logiciels
+  include fichiers
+  include nrpe
+  class { 'nrpe':
     dont_blame_nrpe => '1',
     allowed_hosts => ['172.20.170.32', '172.20.170.12']
-}
+  }
+  }
 
-#class { 'nrpe::plugins' {
-#        'check_apt':
-#                ensure  => present,
-#                sudo    => true,
-#                command => 'check_dell_warranty';
-#    }
 
-#class { 'sudo': }
+
+
+
 
