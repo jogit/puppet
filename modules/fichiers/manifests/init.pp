e56eaa4fd1d3efbedcb5295b26f410f6f0e3a4f6
jogit <jerome.rabouille@gmail.com>
2015-03-17 16:15:52 +0100

    suppression de l'import de fichier.pp

--- modules/fichiers/manifests/init.pp
+++ modules/fichiers/manifests/init.pp
@@ -3,8 +3,8 @@ class fichiers {
   file { "/etc/puppet/puppet.conf":
     owner => root,
     group => root,
-    mode => 0644,
-    source => "files/etc/puppet/puppet.conf",
+    mode => '0644',
+    source => "puppet:///modules/files/etc/puppet/puppet.conf",
   }
 
   #Script pour nettoyer les anciens kernels
@@ -14,9 +14,9 @@ file { "/usr/sbin/purgekernel":
   mode => '0755',
   ensure => present,
   source => $::is_proxmox ? {
-    true => "puppet:///files/usr/sbin/purgekernel_proxmox",
-    false => "puppet:///files/usr/sbin/pugekernel",
-    default => "puppet:///files/usr/sbin/purgekernel_proxmox"
+    true => "puppet:///modules/files/usr/sbin/purgekernel_proxmox",
+    false => "puppet:///modules/files/usr/sbin/pugekernel",
+    default => "puppet:///modules/files/usr/sbin/purgekernel_proxmox"
   }
 }
   file { "/etc/apt/apt.conf.d/88pergekernel":
@@ -26,7 +26,7 @@ file { "/usr/sbin/purgekernel":
     owner => root,
     group => root,
     mode => '0644',
-    source => "puppet:///files/etc/apt/apt.conf.d/88purgekernel",
+    source => "puppet:///modules/files/etc/apt/apt.conf.d/88purgekernel",
     ensure => present,
   }
 
@@ -34,7 +34,7 @@ file { "/usr/sbin/purgekernel":
     owner   => root,
     group   => root,
     mode    => '0644',
-    source  => "puppet:///files/etc/apt/apt.conf",
+    source  => "puppet:///modules/files/etc/apt/apt.conf",
     ensure => present,
 }
 }
