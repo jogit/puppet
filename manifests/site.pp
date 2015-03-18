5b2c608a8ec77bc613147b6a57b23c2feacd4a61
jogit <jerome.rabouille@gmail.com>
2015-03-18 11:56:43 +0100

    config commande NRPE event_ntp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,29 +4,31 @@ File { backup => 'main' }
 Exec { path => "/bin:/sbin:/usr/bin:/usr/sbin" }
 
 import "logiciels"
-#import "fichiers"
 
 
 node default {
   include logiciels
   include fichiers
-  #include nrpe
+
+  #Configuration de NRPE
   class { 'nrpe':
     dont_blame_nrpe => '1',
     allowed_hosts => ['172.20.170.32', '172.20.170.12']
   }
-  
-  }
-  nrpe::plugin { 'event_ntp':
-             # source_prefix => 'puppet:///',
-              source => 'nrpe/event_ntp.sh'
-        }
-        
+
+  #Configuration de SNMP      
   class { 'snmp':
-  agentaddress => [ 'udp:161' ],
-#  com2sec      => [ 'notConfigUser 10.20.30.40/32 SeCrEt' ],
-  contact      => 'pole-informatique.stgi@univ-fcomte.fr',
-  location     => 'UFR STGI Belfort',
-   ro_community      => 'stgi',
-   ro_network   => '172.20.170.32',
-    }
+    agentaddress  => [ 'udp:161' ],
+    contact       => 'pole-informatique.stgi@univ-fcomte.fr',
+    location      => 'UFR STGI Belfort',
+    ro_community  => 'stgi',
+    ro_network    => '172.20.170.32',
+  }
+
+}
+  
+#Configuration des plugins de NRPE
+nrpe::plugin { 'event_ntp':
+             source => 'nrpe/event_ntp.sh'
+}
+  
